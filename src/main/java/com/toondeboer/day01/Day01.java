a180ab41f43887319b34c4f25f2eef2f9973d8c7
Toon de Boer <toondeboer98@icloud.com>
2024-12-11 18:06:46 +0100

    day 1

--- src/main/java/com/toondeboer/day01/Day01.java
+++ src/main/java/com/toondeboer/day01/Day01.java
@@ -2,10 +2,55 @@ package com.toondeboer.day01;
 
 import com.toondeboer.utils.InputReader;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Day01 {
     public static void run() {
-        System.out.println("Day 1 Solution");
         String input = InputReader.readInput("day01.txt");
-        System.out.println(input);
+
+        int distance = solve(input);
+
+        System.out.println("Day 1: " + distance);
+    }
+
+    public static int solve(String input) {
+        List<List<Integer>> lists = getArrays(input);
+
+        return getDistance(lists);
+    }
+
+    private static List<List<Integer>> getArrays(String input) {
+        String[] lines = input.split("\\n");
+        List<Integer> leftList = new ArrayList<>();
+        List<Integer> rightList = new ArrayList<>();
+
+        for (String line : lines) {
+            String[] numbers = line.split("\\s+");
+            leftList.add(Integer.parseInt(numbers[0]));
+            rightList.add(Integer.parseInt(numbers[1]));
+        }
+
+        List<List<Integer>> lists = new ArrayList<>();
+        lists.add(leftList);
+        lists.add(rightList);
+        return lists;
+    }
+
+    private static int getDistance(List<List<Integer>> lists) {
+        List<Integer> leftList = lists.get(0);
+        List<Integer> rightList = lists.get(1);
+
+        Collections.sort(leftList);
+        Collections.sort(rightList);
+
+        int distance = 0;
+
+        for (int i = 0; i < leftList.size(); i++) {
+            distance += Math.abs(leftList.get(i) - rightList.get(i));
+        }
+
+        return distance;
     }
 }
---
+++ src/test/java/com/toondeboer/day01/Day01Test.java
@@ -0,0 +1,19 @@
+package com.toondeboer.day01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day01Test {
+    @Test
+    void test() {
+        String input = """
+                3   4
+                4   3
+                2   5
+                1   3
+                3   9
+                3   3""";
+        assertEquals(11, Day01.solve(input));
+    }
+}
