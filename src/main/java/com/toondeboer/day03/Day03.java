0e71d3da81630de75baeba95bf532b0b107e6e2b
Toon de Boer <toondeboer98@icloud.com>
2024-12-14 14:36:27 +0100

    day 3, part 2

--- src/main/java/com/toondeboer/day03/Day03.java
+++ src/main/java/com/toondeboer/day03/Day03.java
@@ -12,15 +12,18 @@ public class Day03 {
     public static void run() {
         String input = InputReader.readInput("day03.txt");
 
-        int sum = solve(input);
+        int[] sum = solve(input);
 
-        System.out.println("Day 3: " + sum);
+        System.out.println("Day 3: part 1: " + sum[0] + ", part 2: " + sum[1]);
     }
 
-    public static int solve(String input) {
-        List<String> multiplications = getMultiplicationStrings(input);
+    public static int[] solve(String input) {
+        List<String> multiplicationsPart1 = getMultiplicationStrings(input);
+        int sumPart1 = sumOfMultiplications(multiplicationsPart1);
 
-        return sumOfMultiplications(multiplications);
+        int sumPart2 = sumOfMultiplicationsPart2(input);
+
+        return new int[]{sumPart1, sumPart2};
     }
 
     private static List<String> getMultiplicationStrings(String input) {
@@ -40,13 +43,43 @@ public class Day03 {
     private static int sumOfMultiplications(List<String> multiplications) {
         int sum = 0;
         for (String mul : multiplications) {
-            String[] numbers = mul.replace("mul(", "")
-                    .replace(")", "")
-                    .split(",");
-            int a = Integer.parseInt(numbers[0]);
-            int b = Integer.parseInt(numbers[1]);
-            sum += (a * b);
+            sum += multiply(mul);
         }
+        return sum;
+    }
+
+    private static int multiply(String mul) {
+        String[] numbers = mul.replace("mul(", "")
+                .replace(")", "")
+                .split(",");
+        int a = Integer.parseInt(numbers[0]);
+        int b = Integer.parseInt(numbers[1]);
+        return (a * b);
+    }
+
+    private static int sumOfMultiplicationsPart2(String input) {
+        int sum = 0;
+
+        String multiplicationRegex = "mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)";
+        Pattern pattern = Pattern.compile(multiplicationRegex);
+        Matcher matcher = pattern.matcher(input);
+
+        boolean enabled = true;
+        while (matcher.find()) {
+            String matchedString = matcher.group();
+            if (matchedString.equals("do()")) {
+                enabled = true;
+                continue;
+            }
+            if (matchedString.equals("don't()")) {
+                enabled = false;
+                continue;
+            }
+            if (enabled) {
+                sum += multiply(matchedString);
+            }
+        }
+
         return sum;
     }
 }
--- src/test/java/com/toondeboer/day03/Day03Test.java
+++ src/test/java/com/toondeboer/day03/Day03Test.java
@@ -6,8 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day03Test {
     @Test
-    void test() {
+    void testPart1() {
         String input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-        assertEquals(161, Day03.solve(input));
+        assertEquals(161, Day03.solve(input)[0]);
+    }
+
+    @Test
+    void testPart2() {
+        String input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assertEquals(48, Day03.solve(input)[1]);
     }
 }
